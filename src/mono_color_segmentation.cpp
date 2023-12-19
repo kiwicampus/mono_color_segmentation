ddfe2272fe330cbe2bbddeee02625ecbb61ae20b
pepisg <pedro.gonzalez@eia.edu.co>
2023-12-19 00:03:41 +0000

    Latched labelinfo qos

--- src/mono_color_segmentation.cpp
+++ src/mono_color_segmentation.cpp
@@ -11,19 +11,23 @@ MonoColorSegmentation::MonoColorSegmentation(const rclcpp::NodeOptions & options
         "image_raw", 10, std::bind(&MonoColorSegmentation::imageCallback, this, std::placeholders::_1));
 
     mask_publisher_ = this->create_publisher<sensor_msgs::msg::Image>("mask", 10);
-    label_info_publisher_ = this->create_publisher<vision_msgs::msg::LabelInfo>("label_info", 10);
+    rclcpp::QoS label_info_qos(rclcpp::KeepLast(10));
+    label_info_qos.transient_local();
+    label_info_publisher_ = this->create_publisher<vision_msgs::msg::LabelInfo>(
+        "label_info", label_info_qos);
 
     label_info_.class_map.resize(2);
     label_info_.class_map.at(0).class_name = "others";
     label_info_.class_map.at(0).class_id = 0;
     label_info_.class_map.at(1).class_name = "ground_plane";
     label_info_.class_map.at(1).class_id = 255;
+    label_info_publisher_->publish(label_info_);
 }
 
 void MonoColorSegmentation::imageCallback(const sensor_msgs::msg::Image::SharedPtr msg) {
     createMask(msg);
     label_info_.header = msg->header;
-    label_info_publisher_->publish(label_info_);
+    
 }
 
 void MonoColorSegmentation::createMask(const sensor_msgs::msg::Image::SharedPtr& image) {
